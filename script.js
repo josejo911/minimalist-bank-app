4e3465c859d6b7f4dd3452fd3b5a8cb71c7e2cfe
Javier <jo14343@uvg.edu.gt>
2022-05-11 23:39:38 -0600

    Avances en app
    Se agregan multiples funcionalidades del portal

--- script.js
+++ script.js
@@ -72,3 +72,248 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const displayMovements = function (movements) {
+  movements.forEach(function (mov, i) {
+    // vaciamos el contenido actual del container
+    containerMovements.innetHTML = '';
+
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${mov}€</div>
+    </div>
+    `;
+    // aca agregamos el movimiento en la parte superior del container
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+// displayMovements(account1.movements);
+
+const calcPrintBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance} EUR`;
+};
+// calcPrintBalance(account1.movements);
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const outcomes = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+// calcDisplaySummary(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    // creamos el username en el objeto para cada cuentaviente en el array
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+  // creamos el username, el map siempre necesita regresar el valor del nuevo array
+};
+createUsernames(accounts);
+
+const updateUI = function (acc) {
+  // display movements
+  displayMovements(acc.movements);
+
+  // display balance
+  calcPrintBalance(acc);
+  // display summary
+  calcDisplaySummary(acc);
+};
+// Event handler
+let currentAccount;
+btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  // optional chaining currentAccount?
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // display UI and welcome message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+    // clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+    // update ui
+    updateUI(currentAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    //update ui
+    updateUI(currentAccount);
+  }
+});
+
+// LOAN FUNCTIONALITU
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+  // soem method
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // add movement
+    currentAccount.movements.push(amount);
+    // update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+// FIND INDEX
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    // identificamos el index de la cuenta actual para poder compararla y posteriormente borrarla. findIndex necesita una respuesta booleana
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    //delete account
+    accounts.splice(index, 1);
+    // HIDE UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
+//Includes vs SOME, includes se usa cuando queremos buscar un valor en un listado, some puede utilizar una condicion para la busqueda.
+// console.log(movements.includes(-130))
+// console.log(movements.some(mov => mov === -130));
+// console.log(movements.some(mov => mov  > 0));
+
+//EVERY METHOD
+// console.log(movements.every(mov => mov > 0));
+// console.log(account4.movements.every(mov => mov > 0));
+
+// SEPARATE CALLBACK
+// const deposit = mov => mov > 0;
+// console.log(movements.some(deposit));
+// console.log(movements.every(deposit));
+// console.log(movements.filter(deposit));
+
+// FLAT AND FLATMAP
+
+// const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+// console.log(arr.flat());
+// const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+// flat(niveles de deeping)
+// console.log(arrDeep.flat(2));
+// SOLICITUD: SUMAR TODOS LOS MOVIMIENTOS DE TODAS LAS CUENTAS
+
+// OPCION OBLIGATORIA: CHAINING
+// FLAT
+const overallBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+
+//FLAT MAP: El problema que solo va un nivel de profundidad para mas niveles usar FLAT
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance2);
+
+//FIND METHOD: find method needs callback must be boolean, return de first element que cumple la condicion a diferencia que el filter regresa el array de elementos.
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+
+// use Find in object accounts
+const account = accounts.find(acc => acc.owner === 'Pedro Picapiedra');
+// console.log(account);
+
+//--------------------------------
+const eurToUsd = 1.1;
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+
+//MAXIMUM VALUE OF THE MOVEMENTS  ARRAY
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) {
+    return acc;
+  } else {
+    return mov;
+  }
+}, movements[0]);
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+
+const withdrawals = movements.filter(mov => mov < 0);
+
+// console.log(movements);
+// accumulator is like SNOWBALL
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+
+// console.log(balance);
+const euroToUsd = 1.1;
+
+// map necesita un callback
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * euroToUsd;
+// });
+// parameters => return operation {}
+const movementsUSD = movements.map(mov => mov * euroToUsd);
+
+const movementsDescriptions = movements.map((mov, i) => {
+  `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+    mov
+  )}`;
+});
